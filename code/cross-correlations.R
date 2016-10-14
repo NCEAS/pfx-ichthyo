4c2c66efba65bc8b3d29cbee9ea23a249acb2b78
Sean Anderson <sean@seananderson.ca>
2016-10-14 14:22:38 -0700

    Add rank-order r

--- code/cross-correlations.R
+++ code/cross-correlations.R
@@ -12,19 +12,34 @@ autocorr_pyper<-function(N,tsx,tsy) {
   ifelse(Nstar<N,Nstar,N)
 }
 
-cc <- function(x, y, k, trans = I, pyper = FALSE, alpha = 0.9) {
+cc <- function(x, y, k, trans = I, pyper = FALSE, alpha = 0.9, method = "pearson", nboot = 200L) {
   plyr::ldply(seq(1, nrow(responses) - k + 1), function(i) {
     xdata <-  trans(predictors[i:(k + i),x])
     ydata <-  trans(responses[i:(k + i),y])
-    m <- cor.test(xdata, ydata, use = "complete.obs")
-    r <- m$estimate[[1]]
-    if (!pyper) {
-      l <- m$conf.int[[1]]
-      u <- m$conf.int[[2]]
-    } else {
-      n.star <- autocorr_pyper(N = length(xdata), tsx = xdata, tsy = ydata)
-      l <- psychometric::CIr(r=r, n = n.star, level = alpha)[1]
-      u <- psychometric::CIr(r=r, n = n.star, level = alpha)[2]
+    
+    if (method == "pearson") {
+      m <- cor.test(xdata, ydata, use = "complete.obs", method = method)
+      r <- m$estimate[[1]]
+      if (!pyper) {
+        l <- m$conf.int[[1]]
+        u <- m$conf.int[[2]]
+      } else {
+        n.star <- autocorr_pyper(N = length(xdata), tsx = xdata, tsy = ydata)
+        l <- psychometric::CIr(r=r, n = n.star, level = alpha)[1]
+        u <- psychometric::CIr(r=r, n = n.star, level = alpha)[2]
+      }
+    } else { # not pearson
+      f <- function(data, i) cor(data[i, 1], data[i, 2], method = method)
+      b <- boot::boot(cbind(xdata, ydata), f, nboot)
+      ci <- tryCatch({boot::boot.ci(b)}, error = function(e) NA)
+      if (!is.na(ci)) {
+        l <- ci$bca[[4]]
+        u <- ci$bca[[5]]
+      } else {
+        l <- -1
+        u <- 1
+      }
+      r <- cor.test(xdata, ydata, use = "complete.obs", method = method)$estimate[[1]]
     }
     data.frame(x, y, r, i, l, u)
   })
@@ -33,11 +48,11 @@ cc <- function(x, y, k, trans = I, pyper = FALSE, alpha = 0.9) {
 library(manipulate)
 manipulate({
   ccnames <- expand.grid(x = names(predictors), y = names(responses), k = k)
-  out <- plyr::mdply(ccnames, cc, trans = I, pyper = TRUE)
+  out <- plyr::mdply(ccnames, cc, trans = I, pyper = pyper, method = method)
   
   out <- out %>% mutate(sig = ifelse(u < 0, "low", ifelse(l > 0, "high", "none")))
   
-  ggplot(out, aes(i, r)) + 
+  ggplot(out, aes(i+1980, r)) + 
     geom_line() +
     geom_ribbon(aes(ymin = l, ymax = u), alpha = 0.2) +
     facet_grid(x~y) +
@@ -46,11 +61,10 @@ manipulate({
     theme_light() + guides(colour = FALSE) +
     theme(panel.grid.major = element_blank(),
       panel.grid.minor = element_blank())
-}, k = slider(4, 25, 12))
+}, k = slider(4, 25, 12), method = picker("pearson", "spearman", "kendall"), pyper = picker(TRUE, FALSE))
 
-
-# d %>% mutate(year = seq_len(nrow(d))) %>% 
-#   reshape2::melt(id = "year") %>% 
-#     ggplot(aes(year, value)) + 
-#       geom_line() +
-#       facet_wrap(~variable, scales = "free_y")
+d %>% mutate(year = seq_len(nrow(d))) %>%
+  reshape2::melt(id = "year") %>%
+    ggplot(aes(year, value)) +
+      geom_line() +
+      facet_wrap(~variable, scales = "free_y")
