89d50b3927ab220c392df7145e5cab905545b72f
Sean Anderson <sean@seananderson.ca>
2016-10-14 09:18:21 -0700

    Indent code, tweak plot

--- code/cross-correlations.R
+++ code/cross-correlations.R
@@ -14,37 +14,39 @@ autocorr_pyper<-function(N,tsx,tsy) {
 
 cc <- function(x, y, k, trans = I, pyper = FALSE, alpha = 0.9) {
   plyr::ldply(seq(1, nrow(responses) - k + 1), function(i) {
-  xdata <-  trans(predictors[i:(k + i),x])
-  ydata <-  trans(responses[i:(k + i),y])
-  m <- cor.test(xdata, ydata, use = "complete.obs")
-  r <- m$estimate[[1]]
-  if (!pyper) {
-  l <- m$conf.int[[1]]
-  u <- m$conf.int[[2]]
-  } else {
-   n.star <- autocorr_pyper(N = length(xdata), tsx = xdata, tsy = ydata)
-   l <- psychometric::CIr(r=r, n = n.star, level = alpha)[1]
-   u <- psychometric::CIr(r=r, n = n.star, level = alpha)[2]
-  }
-  data.frame(x, y, r, i, l, u)
-})
+    xdata <-  trans(predictors[i:(k + i),x])
+    ydata <-  trans(responses[i:(k + i),y])
+    m <- cor.test(xdata, ydata, use = "complete.obs")
+    r <- m$estimate[[1]]
+    if (!pyper) {
+      l <- m$conf.int[[1]]
+      u <- m$conf.int[[2]]
+    } else {
+      n.star <- autocorr_pyper(N = length(xdata), tsx = xdata, tsy = ydata)
+      l <- psychometric::CIr(r=r, n = n.star, level = alpha)[1]
+      u <- psychometric::CIr(r=r, n = n.star, level = alpha)[2]
+    }
+    data.frame(x, y, r, i, l, u)
+  })
 }
 
 library(manipulate)
 manipulate({
-ccnames <- expand.grid(x = names(predictors), y = names(responses), k = k)
-out <- plyr::mdply(ccnames, cc, trans = I, pyper = TRUE)
-
-out <- out %>% mutate(sig = ifelse(u < 0, "low", ifelse(l > 0, "high", "none")))
-
-ggplot(out, aes(i, r)) + 
-  geom_line() +
-  geom_ribbon(aes(ymin = l, ymax = u), alpha = 0.2) +
-  facet_grid(x~y) +
-  geom_hline(yintercept = 0, lty = 2, colour = "grey50") +
-  geom_point(data = filter(out, sig != "none"), aes(color = sig)) +
-  theme_light()
-}, k = slider(4, 25, 8))
+  ccnames <- expand.grid(x = names(predictors), y = names(responses), k = k)
+  out <- plyr::mdply(ccnames, cc, trans = I, pyper = TRUE)
+  
+  out <- out %>% mutate(sig = ifelse(u < 0, "low", ifelse(l > 0, "high", "none")))
+  
+  ggplot(out, aes(i, r)) + 
+    geom_line() +
+    geom_ribbon(aes(ymin = l, ymax = u), alpha = 0.2) +
+    facet_grid(x~y) +
+    geom_hline(yintercept = 0, lty = 2, colour = "grey50") +
+    geom_point(data = filter(out, sig != "none"), aes(color = sig)) +
+    theme_light() + guides(colour = FALSE) +
+    theme(panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank())
+}, k = slider(4, 25, 12))
 
 
 # d %>% mutate(year = seq_len(nrow(d))) %>% 
